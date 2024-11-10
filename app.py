9035c70bb71ad1fac86a33b0063b64e6c01c185f
Poisonlocket <loribono@gmx.ch>
2024-11-10 18:23:20 +0100

    B2G implemented, im gonna hang myself

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, Response, request
-from math import pow
+
+from number import Number
+
+
+from palindrome import palindrome_check
 
 app = Flask("app")
 
@@ -15,23 +19,39 @@ def teapot() -> Response:
     """
     return Response(tea, mimetype="text/plain")
 
-
+# A
 @app.post("/calc/func")
 def calc_func() -> Response:
     data = request.get_json()
     num_to_square = data["number"]
     result = num_to_square ** 2
-    return Response(result, 200)
+    number = str(result)
+    return Response(number, 200)
 
 @app.post("/calc/oop")
 def calc_oop() -> Response:
     data = request.get_json()
     num_to_square = data["number"]
-    num_to_square.pow(2)
+    num_to_square = Number(value=num_to_square)
+    num_to_square=num_to_square.square()
+    num_to_square = str(num_to_square)
     return Response(num_to_square, 200)
 
+# B1
+@app.post("/palindrome")
+def palindrome() -> Response:
+    data = request.get_json()
+    word=data["word"]
+    if palindrome_check(word):
+        return Response(f"{word} is a palindrome", 200)
+    else:
+        return Response(f"{word} is not a palindrome", 200)
 
-
-
+# B2G
+@app.get("/greet/<name>")
+def greeting(name:str) -> Response:
+    greet = lambda name: f"Hello, {name}!"
+    return Response(greet(name))
 app.run()
 
+#B2F
